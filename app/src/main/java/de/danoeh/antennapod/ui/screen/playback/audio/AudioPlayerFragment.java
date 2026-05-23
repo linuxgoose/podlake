f682a987321d6718427066f908d084534efb9a8b
Jordan Robinson <hello@jordanrobinson.org>
2026-05-23 10:58:42 +0100

    Revert "clean up"
    This reverts commit 87d2b16acf65f512c6850a737aa8d69726ce976d.

--- app/src/main/java/de/danoeh/antennapod/ui/screen/playback/audio/AudioPlayerFragment.java
+++ app/src/main/java/de/danoeh/antennapod/ui/screen/playback/audio/AudioPlayerFragment.java
@@ -18,6 +18,8 @@ import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.interpolator.view.animation.FastOutSlowInInterpolator;
 import androidx.media3.session.MediaController;
+import androidx.media3.session.SessionCommand;
+import androidx.media3.common.util.Util;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
